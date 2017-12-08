a99e59171728949a3dd5a70b09818b215223d998
John Vaccaro <vaccaro.john@macbookair-2017092.local>
2017-12-07 19:52:27 -0600

    Get rid of secret key

--- Leonard/Leonard/settings.py
+++ Leonard/Leonard/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '9&c+iv5=$ien(4p6pa*l1z^cogsz^lf_f5its!@ag%_+a9d5=0')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
