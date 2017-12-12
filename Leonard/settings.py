90e20941ce05f089377ac1ea22185e7ad61870f0
John Vaccaro <vaccaro.john@macbookair-2017092.local>
2017-12-11 20:04:44 -0600

    Added Dictionary password validator to project

--- Leonard/settings.py
+++ Leonard/settings.py
@@ -98,6 +98,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+    {
+        'NAME': 'main.password_validation.DictionaryPasswordValidator',
+    },
 ]
 
 
